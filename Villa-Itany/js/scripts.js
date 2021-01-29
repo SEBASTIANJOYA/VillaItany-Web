a6c03f356ee395be704254d23d5957871019275f
SEBASTIANJOYA <johann.joya@uptc.edu.co>
2021-01-29 13:04:50 -0500

    Cambios Reservas

--- Villa-Itany/js/scripts.js
+++ Villa-Itany/js/scripts.js
@@ -50,3 +50,58 @@ window.addEventListener('load', () => {
 
 })
 
+let numrandom = 0;
+let computador = {
+
+    nombre:"TOSHIBA",
+    procesador:"INTEL-CORE",
+    ram: "16GB"
+
+};
+ 
+arrayone = ["Sebas","Jorge","Carlos"];
+arraytwo = ["Mario","Karla",arrayone];
+
+for(let array in arraytwo){
+
+    if(array == 2){
+        for(let array of arrayone){
+          document.write(array);
+        }
+    }else{
+
+        document.write(arraytwo[array]);
+    }
+}
+
+document.write("<br>");
+
+let nombre = computador["nombre"];
+let procesador = computador["procesador"];
+let ram = computador["ram"];
+
+frase = " NOMBRE : " + nombre + " PROCESADOR : " + procesador + " RAM : " + ram + "  <br>  ";
+
+document.write(frase);
+
+while(numrandom < 100){
+
+    numrandom = numrandom + 1;
+    document.write(numrandom);
+    document.write("<br>");
+
+    if(numrandom == 8){
+
+        break;
+
+    }
+}
+
+for(let i = 0; i < 10; i++){
+
+    if(i == 3 || i == 6 || i == 9){
+        continue;
+    }
+
+    document.write(i);
+}
