374b61fe1dda72023828c09914863daca83a4c6e
SEBASTIANJOYA <johann.joya@uptc.edu.co>
2021-01-29 21:41:13 -0500

    Validacion Formulario Javascript

--- Villa-Itany/js/scripts.js
+++ Villa-Itany/js/scripts.js
@@ -48,60 +48,78 @@ window.addEventListener('load', () => {
         }
     }
 
-})
+})   ;
 
-let numrandom = 0;
-let computador = {
+//CALCULA LA EDAD DE EL QUE ESTA RESERVANDO.
 
-    nombre:"TOSHIBA",
-    procesador:"INTEL-CORE",
-    ram: "16GB"
+var age = function( dateBegin, dateEnd ){
+    var years = dateEnd.getFullYear() - dateBegin.getFullYear();
+    const months = dateEnd.getMonth() - dateBegin.getMonth();
+    const days = dateEnd.getDate() - dateBegin.getDate();
 
-};
+    if( months < 0 || ( months == 0 && days < 0 ) ){
+        years--;
+    }
+
+    return( years)
+}
+
+//ALMACENA LA CANTIDAD DE LA EDAD EN EL INPUT DE LA EDAD.
+
+function calcAge(){
+    const dateNow = new Date();
+    const dateAux = new Date(document.getElementById('dateBirthday').value );
+
+    document.getElementById('edad').value = age( dateAux, dateNow )
+}
+
+//Funcion Calcula Monto a Pagar
+var precio = function( dateBegin, dateEnd ){
+
+    //Calculo diferencia en dias de las dos fechas.
+    var diferencias = dateEnd.getTime() - dateBegin.getTime();
+    var contdias = Math.round(diferencias/(1000*60*60*24));
+
+    var totalpagar = contdias * 38000;
+    return(totalpagar);
+}
+
+//ALMACENA CANTIDAD A PAGAR EN EL INPUT CORRESPONDIENTE
+function calcMontoPagar(){
+
+    const dateinicio = new Date(document.getElementById('dateingreso').value);
+    const datefin = new Date(document.getElementById('datesalida').value);
+
+    
+    
+    document.getElementById('total').value = precio(dateinicio,datefin);
+}
+
+function idReserva(){
+
+    
+}
+
+//BLOQUEA EL TECLADO, SOLO ADMITE NUMEROS.
+function onlyNums(e){
+    const code = window.event ? e.which : e.keyCode;
+
+    return !( code < 48 || code > 57 );
+}
+
+
+$(document).ready(function(){
+	var altura = $('.menu').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() >altura) {
+            $('.menu').addClass('menu-fixed');
+            
+		} else {
+			$('.menu').removeClass('menu-fixed');
+		}
+	});
  
-arrayone = ["Sebas","Jorge","Carlos"];
-arraytwo = ["Mario","Karla",arrayone];
+});
 
-for(let array in arraytwo){
-
-    if(array == 2){
-        for(let array of arrayone){
-          document.write(array);
-        }
-    }else{
-
-        document.write(arraytwo[array]);
-    }
-}
-
-document.write("<br>");
-
-let nombre = computador["nombre"];
-let procesador = computador["procesador"];
-let ram = computador["ram"];
-
-frase = " NOMBRE : " + nombre + " PROCESADOR : " + procesador + " RAM : " + ram + "  <br>  ";
-
-document.write(frase);
-
-while(numrandom < 100){
-
-    numrandom = numrandom + 1;
-    document.write(numrandom);
-    document.write("<br>");
-
-    if(numrandom == 8){
-
-        break;
-
-    }
-}
-
-for(let i = 0; i < 10; i++){
-
-    if(i == 3 || i == 6 || i == 9){
-        continue;
-    }
-
-    document.write(i);
-}
+//VALIDAR SI ESTA OCUPADO EN ESAS FECHAS.
